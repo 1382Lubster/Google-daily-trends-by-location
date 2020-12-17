e32a754cd4ad688bd682b3ba5e00618e57174a7b
1382Lubster <39346213+1382Lubster@users.noreply.github.com>
2020-12-17 14:25:09 +0000

    Update googletrends.py

--- googletrends.py
+++ googletrends.py
@@ -7,18 +7,17 @@ from pytrends.request import TrendReq
 pytrend = TrendReq()
 
 
-
-
-if __name__ == "__main__":
-	# Get Google Hot Trends data
-	loc = sys.argv[1]
-	print("Trends in ",loc)
-
+def Gtrends(loc):
 	df = pytrend.today_searches(pn=loc)
 	print(df.head(50))
 	result = df.to_json(orient='index')
 	parsed = json.loads(result)
-	 
-	
-with open("google_{}_trends.json".format(loc),"w") as wp:
-	wp.write(json.dumps(parsed, indent=4))
+	return parsed
+
+if __name__ == "__main__":
+	loc = sys.argv[1]
+	# Get Google Hot Trends data
+	parsed=Gtrends(loc)	
+	print("Trends in ",loc)
+	with open("google_{}_trends.json".format(loc),"w") as wp:
+		wp.write(json.dumps(parsed, indent=4))
